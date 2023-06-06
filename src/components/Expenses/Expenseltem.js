c350832345582f19fa37e56ecc26efc64d89da3c
Yorvin Luna Wisky <yorvin.luna@micm.gob.do>
2023-06-06 15:59:45 -0400

    Outputting conditional content.

--- src/components/Expenses/Expenseltem.js
+++ src/components/Expenses/Expenseltem.js
@@ -3,16 +3,13 @@ import ExpenseDate from "./ExpenseDate";
 import './ExpenseItem.css';
 import Card from "../UI/Card";
 
-const ExpenseItem = (props) => {
-   
-    const [title, setTitle] = useState(props.title);
-
+const ExpenseItem = ({ title, date, amount }) => {
     return (
       <Card className="expense-item">
-          <ExpenseDate date={props.date}/>
+          <ExpenseDate date={date}/>
           <div className="expense-item__description">
               <h2>{ title }</h2>
-              <div className="expense-item__price">${ props.amount }</div>
+              <div className="expense-item__price">${ amount }</div>
           </div>
       </Card>
     );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,22 +4,34 @@ import Card from "../UI/Card";
 import './Expenses.css'
 import React, { useState } from "react";
 
-const Expenses = (props) => {
+const Expenses = ({items}) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   }
+
+  const filteredExpenses = items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense =>
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />);
+  }
+
     return (
       <div>
         <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          {props.items.map(expense =>
-            <ExpenseItem
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />)}
+          {expensesContent}
         </Card>
       </div>
 
